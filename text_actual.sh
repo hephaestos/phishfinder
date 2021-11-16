a95c527a194c1fbc77985cbf381ac32fab1bc514
Brandon Thomas <thombran@mail.gvsu.edu>
2021-11-16 18:12:17 -0500

    Fixed temp file not being removed

--- text_actual.sh
+++ text_actual.sh
@@ -3,4 +3,5 @@ args=("$@")
 ./email_cleaner_3000_xtra_fast.awk ${args[0]} > temp.txt
 first=`sed -n '1p' temp.txt`
 second=`sed -n '2p' temp.txt`
-sed -n "${first},${second}p" ${args[0]} 
+sed -n "${first},${second}p" ${args[0]}
+rm temp.txt 
