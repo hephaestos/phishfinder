aaa16ca5e18824ea12741e1523254a0b65739d51
Brandon Thomas <thombran@mail.gvsu.edu>
2021-11-30 00:34:47 -0500

    change build script to parse into test & train folders

--- build_data.sh
+++ build_data.sh
@@ -1,8 +1,18 @@
 #! /bin/bash 
 rm -rf data
 rm -rf data/real
+rm -rf data/test_phish
+rm -rf data/train_phish
+rm -rf data/test_real
+rm -rf data/train_real
+
 mkdir data
 mkdir data/real
+mkdir data/test_phish
+mkdir data/train_phish
+mkdir data/test_real
+mkdir data/train_real
+
 for f in mbox/*.mbox ; do
     name=${f#*/}
     name=${name%.*}
@@ -23,4 +33,14 @@ for f in data/real/* ; do
 done
 
 rm data/real/real03951 # Remove remaining Enron data
-rm data/real/real03951 # Remove blank first file
+rm data/real/real00000 # Remove blank first file
+COUNT=0
+for f in data/p*/* ; do
+    if [ $COUNT -lt 3160 ]
+    then
+       mv $f data/train_phish/phish$COUNT
+    else
+       mv $f data/test_phish/phish$COUNT
+    fi
+    COUNT=$((COUNT+1))
+done
